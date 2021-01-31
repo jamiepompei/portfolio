b1b6371e211acaf1bbfa1c382dd4077beddc3c1f
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-30 22:07:37 -0500

    added external link directive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ActivatedRouteSnapshot } from '@angular/router';
+import { ExternalurlDirective } from './externalurl.directive';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: '',
-component: HomeComponent}
+component: HomeComponent},
 ];
 
 @NgModule({
+  providers: [
+  ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
---
+++ src/app/externalurl.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ExternalurlDirective } from './externalurl.directive';
+
+describe('ExternalurlDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ExternalurlDirective();
+    expect(directive).toBeTruthy();
+  });
+});
