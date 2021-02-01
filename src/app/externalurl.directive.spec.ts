42004457f989dfd09b3a56f5d1ea6e8955125b7c
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-02-01 13:57:38 -0500

    removed external URL class

--- src/app/externalurl.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ExternalurlDirective } from './externalurl.directive';
-
-describe('ExternalurlDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ExternalurlDirective();
-    expect(directive).toBeTruthy();
-  });
-});
